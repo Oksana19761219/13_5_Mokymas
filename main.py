b0a583f9b6efcf256e27034abfa12da2bfd3e535
Oksana19761219 <o.valioniene@gmail.com>
2021-12-23 21:08:14 +0200

    idetas projektas

---
+++ main.py
@@ -0,0 +1,7 @@
+from modules.python_kursas import Kursas, PythonKursas
+
+matematikos_kursas = Kursas("matematika", "Antanas Petraitis", 180)
+pythono_kursas = PythonKursas("python", "Petras Antanaitis", 480)
+
+matematikos_kursas.destyti()
+pythono_kursas.destyti()
